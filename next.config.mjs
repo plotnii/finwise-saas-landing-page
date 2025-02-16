78585cc2d2278e1532b2f6a0559215e5aa905d99
plotnii <plotnea40kt@gmail.com>
2025-02-16 21:06:07 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // i18n: {
-  //   locales: ['en', 'ru', 'ro'],
-  //   defaultLocale: 'en',
-  // },
+  i18n: {
+    locales: ['en', 'ru', 'ro'],  // Поддерживаемые языки
+    defaultLocale: 'en',  // Язык по умолчанию
+  },
   reactStrictMode: true,
+  webpack(config) {
+    config.resolve.alias['@locales'] = path.resolve(__dirname, 'src/locales');
+    return config;
+  },
 };
 
 export default nextConfig;
