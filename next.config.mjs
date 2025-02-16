ed4961935532450b037ba58bdf866a19668e991e
plotnii <plotnea40kt@gmail.com>
2025-02-16 21:22:53 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,7 @@
-import path from 'path';  // Импортируем path для работы с путями
-import { fileURLToPath } from 'url';  // Импортируем для работы с url
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-//  i18n: {
+export default {
+  i18n: {
     locales: ['en', 'ru', 'ro'],  // Поддерживаемые языки
     defaultLocale: 'en',  // Язык по умолчанию
   },
-  reactStrictMode: true,
-  webpack(config) {
-    const __filename = fileURLToPath(import.meta.url);  // Получаем путь к текущему файлу
-    const __dirname = path.dirname(__filename);  // Получаем директорию текущего файла
-
-    config.resolve.alias['@locales'] = path.resolve(__dirname, 'src/locales');
-    return config;
-  },
+  reactStrictMode: true
 };
-
-export default nextConfig;
