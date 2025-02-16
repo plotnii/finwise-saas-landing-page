a7bfd73e044c58b429af3249b12e241c133af584
plotnii <plotnea40kt@gmail.com>
2025-02-16 20:53:39 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   i18n: {
-    locales: ['en', 'ru', 'ro'], // Поддерживаемые языки
-    defaultLocale: 'en', // Язык поумолчанию
+    locales: ['en', 'ru', 'ro'],  // Поддерживаемые языки
+    defaultLocale: 'en',  // Язык по умолчанию
   },
+  reactStrictMode: true,  // Включаем строгий режим (по желанию)
 };
 
 export default nextConfig;
